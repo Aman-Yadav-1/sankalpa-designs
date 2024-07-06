6f2117ac32f8c99c85030626cd9a51739561b73f
Aman Yadav <107786152+Aman-Yadav-1@users.noreply.github.com>
2024-07-06 16:11:36 +0530

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -42,7 +42,7 @@ const Header = () => {
               height={50}
             />
             {isScrolled && (
-              <span className="text-xl font-sans1 font-medium mt-3">Sankalpa Designs</span>
+              <span className="text-xl text-blue-800 font-sans1 font-medium mt-3">Sankalpa Designs</span>
             )}
           </div>
         </Link>
